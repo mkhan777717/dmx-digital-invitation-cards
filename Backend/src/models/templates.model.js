295aff780ece3dfc17ddff425dccbe468a4ee92a
Saksham Jain <sakshamjain.officialuser@gmail.com>
2026-07-09 03:41:28 +0530

    feat: add template controllers and invitation CRUD (create, get)

--- Backend/src/models/templates.model.js
+++ Backend/src/models/templates.model.js
@@ -2,33 +2,75 @@ const mongoose = require("mongoose")
 
 const templateSchema = new mongoose.Schema({
     title: {
-        Type: String,
+        type: String,
         required: true
     },
 
     category: {
-        Type: String,
+        type: String,
         required: true
     },
 
     previewImage: {
-        Type: String,
+        type: String,
         required: true
     },
 
     price: {
-        Type: String,
+        type: Number,
         required: true
     },
 
     templateCode: {
-        Type: String,
+        type: String,
         required: true
-    }
+    },
+
+    fields: [
+        {
+            name: {
+                type: String,
+                required: true
+            },
+
+            label: {
+                type: String,
+                required: true
+            },
+
+            type: {
+                type: String,
+                enum: [
+                    "text",
+                    "textarea",
+                    "number",
+                    "date",
+                    "time",
+                    "image",
+                    "music",
+                    "url"
+                ],
+                required: true
+            },
+
+            required: {
+                type: Boolean,
+                default: false
+            },
+
+            placeholder: {
+                type: String
+            },
+
+            order: {
+                type: Number
+            }
+        }
+    ]
 },{
     timestamps: true
 })
 
-const templates = mongoose.model("templates",templateSchema);
+const templates = mongoose.model("Template",templateSchema);
 
 module.exports = templates
